9ffe08a9159413cec124a8205733d3b0b1171adb
hissing-sid <86068841+hissing-sid@users.noreply.github.com>
2022-02-20 20:59:24 +0000

    Initial commit

---
+++ vpc/vpc/vpc_stack.py
@@ -0,0 +1,10 @@
+import aws_cdk as cdk
+import aws_cdk.aws_s3 as s3
+import aws_cdk.aws_ec2 as ec2
+            
+class VpcStack(Stack):
+
+    def __init__(self, scope: cdk.App, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        vpc = ec2.Vpc(self, "TheVPC", cidr="10.0.0.0/16" )
