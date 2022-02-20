0bbccff152d5bc8aab2d960eba6d3314181c0659
hissing-sid <86068841+hissing-sid@users.noreply.github.com>
2022-02-20 21:25:47 +0000

    fixed VpcStack class

--- vpc/vpc/vpc_stack.py
+++ vpc/vpc/vpc_stack.py
@@ -1,7 +1,7 @@
 import aws_cdk as cdk
 import aws_cdk.aws_ec2 as ec2
             
-class VpcStack(Stack):
+class VpcStack(cdk.Stack):
 
     def __init__(self, scope: cdk.App, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
