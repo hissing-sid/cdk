9c4b6186a64f3406f6be639a1231a1b634e8f544
hissing-sid <86068841+hissing-sid@users.noreply.github.com>
2022-02-27 10:02:40 +0000

    Added Interface endpoints for SSM

--- vpc/vpc/vpc_stack.py
+++ vpc/vpc/vpc_stack.py
@@ -14,11 +14,6 @@ class VpcStack(cdk.Stack):
             enable_dns_support=True
         )
 
-        # Send All Flow Logs to S3
-        vpc.add_flow_log("FlowLogS3",
-            destination=ec2.FlowLogDestination.to_s3()
-        )
-
         # Send Flow Logs containing REJECTS to CloudWatch Logs
         vpc.add_flow_log("FlowLogCloudWatch",
             traffic_type=ec2.FlowLogTrafficType.REJECT
@@ -27,9 +22,25 @@ class VpcStack(cdk.Stack):
         security_group = ec2.SecurityGroup(
             self, "CDKSecurityGroup",
             vpc=vpc,
-            allow_all_outbound=None,
+            allow_all_outbound=False,
             description= "Allow SSM Access to instances",
             security_group_name= "CDKSecurityGroup"
         )
 
-        security_group.add_ingress_rule(security_group, ec2.Port.tcp(22) )
+        security_group.add_ingress_rule(security_group, ec2.Port.tcp(443) )
+        security_group.add_egress_rule(security_group, ec2.Port.tcp(443) )
+
+        # Create vpc interface endpoints for SSM, only assigned to the private subnets
+        ssm_endpoint = ec2.InterfaceVpcEndpoint(self, "SSM Endpoint",
+            vpc=vpc,
+            service=ec2.InterfaceVpcEndpointService("com.amazonaws.eu-west-2.ssm", 443),
+            subnets=ec2.SubnetType.PRIVATE_WITH_NAT,
+            security_groups=[security_group]
+        )
+
+        ssm_messages_endpoint = ec2.InterfaceVpcEndpoint(self, "SSM Messages Endpoint",
+            vpc=vpc,
+            service=ec2.InterfaceVpcEndpointService("com.amazonaws.eu-west-2.ssmmessages", 443),
+            subnets=ec2.SubnetType.PRIVATE_WITH_NAT,
+            security_groups=[security_group]
+        )
